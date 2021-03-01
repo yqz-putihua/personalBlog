33f889bff3f6aa89e823b9f443c3a7243e0de3df
zzh <964389099@qq.com>
2021-03-01 20:29:07 +0800

    fsdf

---
+++ src/test/java/com/yqz/pth/personalblog/PersonalblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqz.pth.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
